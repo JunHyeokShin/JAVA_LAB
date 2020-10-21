7669971a11f62e89df30d4d11b5ee384f79b4e26
HYK <junhyeok001010@gmail.com>
2020-10-21 18:22:53 +0900

    Day 1

---
+++ Exercise/src/chapter1/Q6.java
@@ -0,0 +1,7 @@
+package chapter1;
+
+public class Q6 {
+	public static void main(String [] args) {
+		System.out.println("신준혁");
+	}
+}
---
+++ OneMinute/src/chapter2/Variable1.java
@@ -0,0 +1,9 @@
+package chapter2;
+
+public class Variable1 {
+	public static void main(String[] args) {
+		int age;
+		age = 21;
+		System.out.println(age);
+	}
+}
