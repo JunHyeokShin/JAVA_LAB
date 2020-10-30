713bea2e80e68685410a19f11bd4698dcd19528f
HYK <junhyeok001010@gmail.com>
2020-10-31 03:28:31 +0900

    Day 9

---
+++ Exercise/src/chapter6/q6/CardTest.java
@@ -0,0 +1,11 @@
+package chapter6.q6;
+
+public class CardTest {
+	public static void main(String[] args) {
+		Card myCard = new Card();
+		Card yourCard = new Card();
+
+		System.out.println(myCard.getCardNumber());
+		System.out.println(yourCard.getCardNumber());
+	}
+}
