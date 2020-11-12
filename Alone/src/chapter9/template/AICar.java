626067d24b89bcaae486c0c131b4cbac87a6e27b
HYK <junhyeok001010@gmail.com>
2020-11-13 04:34:15 +0900

    Day 16

--- Alone/src/chapter9/template/AICar.java
+++ Alone/src/chapter9/template/AICar.java
@@ -1,4 +1,4 @@
-package template;
+package chapter9.template;
 
 public class AICar extends Car {
 	@Override
@@ -10,5 +10,10 @@ public class AICar extends Car {
 	@Override
 	public void stop() {
 		System.out.println("스스로 멈춥니다.");
+	}
+
+	@Override
+	public void wiper() {
+		System.out.println("비나 눈의 양에 따라 빠르기가 자동으로 조절됩니다.");
 	}
 }
