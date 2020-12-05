cd84ec50af71f98b9f136acf857a925f2a2bb781
HYK <junhyeok001010@gmail.com>
2020-12-06 03:59:59 +0900

    Day 27

---
+++ Chapter14/src/exception/ExceptionHandling1.java
@@ -0,0 +1,14 @@
+package exception;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class ExceptionHandling1 {
+	public static void main(String[] args) {
+		try {
+			FileInputStream fis = new FileInputStream("a.txt");
+		} catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+}
